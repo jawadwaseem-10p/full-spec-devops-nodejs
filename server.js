f8af196eeb1a23a2ed9271b84632e6b979af6eda
jawad <jawad.waseem@hobsons.com>
2020-11-01 22:06:52 +0500

    dockerpusher2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join('./client', 'build')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join('./client', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname+'/client', 'build', 'index.html'))
 })
 // API calls
 app.get('/api/hello', (req, res) => {
